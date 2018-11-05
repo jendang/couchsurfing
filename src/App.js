19965c0cb571f155667d5d2f5a238d3be41204ad
Kyle Gracia <kylepgracia@gmail.com>
2018-11-05 17:30:36 +0100

    component setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import Image from './components/image'
+import Information from './components/information'
+import Yes from './components/yes'
+import No from './components/no'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Couch Surfing</h1>
+          <h1>Couchassier</h1>
         </header>
+
+        <main>
+          <Navbar />
+          <Yes />
+          <Image />
+          <No />
+          <Information /> 
+        </main>
       </div>
     );
   }
---
+++ src/components/No.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class No extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default No;

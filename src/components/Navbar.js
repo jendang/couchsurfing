c34a780088b600bd0d122333408e2513c91ac939
Kyle Gracia <kylepgracia@gmail.com>
2018-11-06 10:51:35 +0100

    added navbar + comp/style dir

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import './styles/navbar.css';
 
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="Nav">
+      
+      <ul>
+
+        <li><a href="https://www.home.com">Home</a></li>
+
+        <li><a href="https://www.preferences.com">Preferences</a></li>
+
+        <li><a href="https://www.messages.com">Messages</a></li>
+
+        <li><a href="https://www.surfer.com">Surfer</a></li>
+
+        <li><a href="https://www.host.com">Host</a></li>
+      
+      </ul>
 
       </div>
+
     );
   }
 }

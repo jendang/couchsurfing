b2136077331d2c49d3348fc9dd313e647be32b40
Kyle Gracia <kylepgracia@gmail.com>
2018-11-07 16:32:31 +0100

    some css changes

--- src/components/LogInContainer.js
+++ src/components/LogInContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './styles/LogInContainer.css';
 
 
 

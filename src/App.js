02d5770fa783335cdb4b1a6b2270dc0572906e4e
Kyle Gracia <kylepgracia@gmail.com>
2018-11-07 17:22:04 +0100

    reset app css

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
         <div className="App">
         <header className="App-header">
-          <h1>Couchaisseur</h1>
+          <Navbar />
         </header>
 
         <main>

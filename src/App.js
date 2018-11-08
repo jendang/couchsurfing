d2d0311cdfbe333dca6239d7022614471d93e17f
Kyle Gracia <kylepgracia@gmail.com>
2018-11-08 10:46:30 +0100

    imagecss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Yes from './components/Yes'
-import No from './components/No'
 import Navbar from './components/Navbar'
 import InformationHostsContainer from './components/InformationHostsContainer';
 import InformationSurfersContainer from './components/InformationSurfersContainer';
 import LogInContainer from './components/LogInContainer';
 import Match from './components/pushMatch';
+import Image from './components/Image';
 import { BrowserRouter } from 'react-router-dom'
 import {Route} from 'react-router-dom'
 
@@ -28,11 +27,8 @@ class App extends Component {
 
         <main>
           
-          {/* <Navbar /> 
-          <Yes />
-          <Image />
-          <No /> */}
 
+          <Route exact path="/surfers/" component={Image} />
           <Route exact path="/" component={LogInContainer} />
           <Route exact path="/surfers/" component={InformationSurfersContainer} />
           <Route exact path="/hosts/" component={InformationHostsContainer} /> 
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Yes from './Yes'
+import No from './No'
+import './styles/image.css';
 
 
 class Image extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="flex-container">
+
+        <div><div className="button"><No /></div></div>
+
+        <div><img width="250px" alt="profilepic" src="images/768px-Halbe_Zijlstra_2012.jpg"></img></div>
+
+        <div><div className="button"><Yes /></div></div>
 
       </div>
     );

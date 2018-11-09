c0ac32a709bcc884d05764eb580126f4863e996e
JENDANG <JENNY.DANG117@GMAIL.COM>
2018-11-09 11:51:18 +0100

    cleaning

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import './App.css';
 import InformationHostsContainer from './components/InformationHostsContainer';
 import InformationSurfersContainer from './components/InformationSurfersContainer';
 import LogInContainer from './components/LogInContainer';
-
 import { BrowserRouter } from 'react-router-dom'
 import {Route} from 'react-router-dom'
-
 import InitialButton from './components/InitialButton'
 import MatchingHost from './components/MatchingHost';
 import MatchingSurfer from './components/MatchingSurfer';
-
-
-
-
 
 
 class App extends Component {
@@ -22,34 +16,21 @@ class App extends Component {
 
   render() {
     return (
-        <BrowserRouter>
-
-        <div className="App">
-        <header className="App-header">
-        </header>
+    <BrowserRouter>
+      <div className="App">
+          <header className="App-header">
+          </header>
 
         <main>
-          
-
-
-
-
-
-
           <Route exact path="/" component={LogInContainer} />
           <Route exact path="/users/" component={InitialButton} />  
           <Route exact path="/surfers/" component={InformationSurfersContainer} />
           <Route exact path="/hosts/" component={InformationHostsContainer} /> 
-
-          {/* <Route exact path="/users/" component={InformationHostsContainer} />   */}
           <Route exact path="/matching-host/" component={MatchingHost} />
           <Route exact path="/matching-surfer/" component={MatchingSurfer} />
-
-          
-          
         </main>
       </div>
-      </BrowserRouter>
+    </BrowserRouter>
     );
   }
 
